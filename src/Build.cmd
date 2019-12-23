b3c0ce44b85b772ccfaa77d8dd5a380476e15a05
mingkuang <mingkuang@live.com>
2019-12-23 17:08:25 +0800

    将__security_cookie设置为weak（感谢 Syrinx）

--- src/Build.cmd
+++ src/Build.cmd
@@ -22,13 +22,15 @@ goto:eof
 cl /O1 /Os /Oi /GS- /arch:IA32 /Z7 /MT /Fo"objs\\%Platform%\\%1" /Zl /c /D "NDEBUG" /D "YY_Thunks_Support_Version=%2" "%~dp0YY_Thunks.cpp"
 
 ::进行函数名称进行修正 __imp__%s_%u -> __imp__%s@%u
-LibMaker.exe FixObj "%~dp0..\\objs\\%Platform%\\%1"
+LibMaker.exe FixObj "%~dp0..\\objs\\%Platform%\\%1" /WeakExternFix:__security_cookie=%PointType%
 
 LibMaker.exe AppendWeak /MACHINE:%Platform% /DEF:"%~dp0def\\%Platform%\\PSAPI2Kernel32.def" /OUT:"%~dp0..\\objs\\%Platform%\\%1"
+
 
 goto:eof
 
 :Buildx86
+set PointType=4
 call:BuildObj YY_Thunks_for_WinXP.obj NTDDI_WINXP
 call:BuildObj YY_Thunks_for_Vista.obj NTDDI_WIN6
 
@@ -37,14 +39,17 @@ goto:eof
 
 
 :Buildx64
+set PointType=8
 call:BuildObj YY_Thunks_for_WinXP.obj NTDDI_WS03SP1
 call:BuildObj YY_Thunks_for_Vista.obj NTDDI_WIN6
 goto:eof
 
 :Buildarm
+set PointType=4
 call:BuildObj YY_Thunks_for_Win8.obj NTDDI_WIN8
 goto:eof
 
 :Buildarm64
+set PointType=8
 call:BuildObj YY_Thunks_for_Win10_RS3.obj NTDDI_WIN10_RS3
 goto:eof
