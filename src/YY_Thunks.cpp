e1272a0d314be3054541ffcf2f91e50333171c20
Yonggang Luo <luoyonggang@gmail.com>
2021-05-18 09:42:22 +0800

    Do not contain Chinese character in macro __YY_Thunks_Expand_Function that confusing the clang-format.

--- src/YY_Thunks.cpp
+++ src/YY_Thunks.cpp
@@ -102,7 +102,7 @@
 	{                                                                                          \
         __declspec(allocate(".YYThr$AAA")) static void* _CRT_CONCATENATE(pInit_ ,_FUNCTION) =  \
               reinterpret_cast<void*>(&_CRT_CONCATENATE(try_get_, _FUNCTION));                 \
-        /*为了避免编译器将 YYThr$AAA 节优化掉*/                                                \
+        /* In order to avoid the compiler optimize section YYThr$AAA out */                    \
         __foreinclude(_CRT_CONCATENATE(pInit_ ,_FUNCTION));                                    \
 		__declspec(allocate(".YYThu$AAB")) static void* _CRT_CONCATENATE(pFun_, _FUNCTION);    \
 		return reinterpret_cast<decltype(_FUNCTION)*>(try_get_function(                        \
