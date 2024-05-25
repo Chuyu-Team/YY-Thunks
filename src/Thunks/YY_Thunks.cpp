35a609cae9a84d2d97d33e17b4793e2d401f9b92
mingkuang <mingkuang@live.com>
2024-05-25 11:16:45 +0800

    Revert "Bug #57, 初始化性能优化，减少无意义的GetProcAddress" (GetProcAddress内部存在锁，为了避免死锁回滚更改)
    This reverts commit e583b944bce8835c28eb96ffcfd59bb8616a2840.

--- src/Thunks/YY_Thunks.cpp
+++ src/Thunks/YY_Thunks.cpp
@@ -166,6 +166,10 @@ RtlCutoverTimeToSystemTime(
 	static decltype(_FUNCTION)* __cdecl _CRT_CONCATENATE(try_get_, _FUNCTION)() noexcept       \
 	{                                                                                          \
         __CHECK_UNIT_TEST_BOOL(_FUNCTION);                                                     \
+        __declspec(allocate(".YYThr$AAA")) static void* _CRT_CONCATENATE(pInit_ ,_FUNCTION) =  \
+              reinterpret_cast<void*>(&_CRT_CONCATENATE(try_get_, _FUNCTION));                 \
+        /*为了避免编译器将 YYThr$AAA 节优化掉*/                                                \
+        __foreinclude(_CRT_CONCATENATE(pInit_ ,_FUNCTION));                                    \
 		__declspec(allocate(".YYThu$AAB")) static void* _CRT_CONCATENATE(pFun_, _FUNCTION);    \
 		return reinterpret_cast<decltype(_FUNCTION)*>(try_get_function(                        \
 		&_CRT_CONCATENATE(pFun_ ,_FUNCTION),                                                   \
