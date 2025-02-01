399806dee10bf322ebbf6075c380d8a17c49c542
HIllya51 <1173718158@qq.com>
2025-02-01 08:07:56 +0800

    Fea, wasapi 环回录制(PR #140)   - 添加 MFCancelWorkItem   - 添加 MFLockSharedWorkQueue   - 添加 MFPutWorkItem   - 添加 MFPutWorkItem2   - 添加 MFPutWaitingWorkItem   - 添加 MFUnlockWorkQueue   - 添加 ActivateAudioInterfaceAsync

---
+++ src/Thunks/mmdevapi.hpp
@@ -0,0 +1,37 @@
+﻿#if (YY_Thunks_Target < __WindowsNT6_2)
+#include <mmdeviceapi.h>
+#endif
+
+namespace YY::Thunks
+{
+#if (YY_Thunks_Target < __WindowsNT6_2)
+
+    // 最低受支持的客户端    Windows 8 [桌面应用 |UWP 应用]
+    // 最低受支持的服务器    Windows Server 2012 [桌面应用 |UWP 应用]
+    __DEFINE_THUNK(
+    mmdevapi,
+    20,
+    HRESULT,
+    STDAPICALLTYPE,
+    ActivateAudioInterfaceAsync,
+        _In_ LPCWSTR _szDeviceInterfacePath,
+        _In_ REFIID riid,
+        _In_opt_ PROPVARIANT* _pActivationParams,
+        _In_ IActivateAudioInterfaceCompletionHandler* _pCompletionHandler,
+        _COM_Outptr_ IActivateAudioInterfaceAsyncOperation** _ppActivationOperation
+        )
+    {
+        if (const auto _pfnActivateAudioInterfaceAsync = try_get_ActivateAudioInterfaceAsync())
+        {
+            return _pfnActivateAudioInterfaceAsync(_szDeviceInterfacePath, riid, _pActivationParams, _pCompletionHandler, _ppActivationOperation);
+        }
+
+        if (!_ppActivationOperation)
+            return E_POINTER;
+
+        *_ppActivationOperation = nullptr;
+        return E_NOTIMPL;
+    }
+#endif
+
+}
